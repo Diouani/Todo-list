391b8638d53d97e786009a636a029adc574318a7
Diouani <73253036+Diouani@users.noreply.github.com>
2021-04-11 20:29:05 +0000

    adding the add and remove items

---
+++ app.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("add-item-form")
+const input = document.getElementById("name-item")
+const itemscontainer = document.getElementById("tqedia-list")
+const clean = document.getElementById("clear")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+Additem()
+})
+
+
+function Additem(item){
+    var inputvalue = input.value
+
+    if(item){
+        inputvalue = item.text
+    }
+if(inputvalue){
+    const displayitem = document.createElement('p')
+    
+        displayitem.classList.add('done')
+   
+    displayitem.innerText = inputvalue
+    itemscontainer.appendChild(displayitem)
+    input.value = ''
+}
+}
+
+clean.addEventListener('click',()=> {
+    document.querySelectorAll(".done").forEach(p => p.remove())
+}
+)
